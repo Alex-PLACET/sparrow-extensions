98045a864a258958a5f99a4d7952dd59d13796e0
Alexis Placet <alexis.placet@quantstack.net>
2025-12-23 11:12:48 +0100

    Use simd json

--- cmake/external_dependencies.cmake
+++ cmake/external_dependencies.cmake
@@ -78,11 +78,22 @@ if(NOT TARGET sparrow::sparrow)
     add_library(sparrow::sparrow ALIAS sparrow)
 endif()
 
+# add sparrow::sparrow to SPARROW_EXTENSIONS_INTERFACE_DEPENDENCIES list
+set(SPARROW_EXTENSIONS_INTERFACE_DEPENDENCIES sparrow::sparrow)
+
+find_package_or_fetch(
+    PACKAGE_NAME simdjson
+    GIT_REPOSITORY https://github.com/simdjson/simdjson.git
+    TAG v2.4.12
+)
+
+set(SPARROW_EXTENSIONS_INTERFACE_DEPENDENCIES ${SPARROW_EXTENSIONS_INTERFACE_DEPENDENCIES} simdjson::simdjson)
+
 if(SPARROW_EXTENSIONS_BUILD_TESTS)
     find_package_or_fetch(
         PACKAGE_NAME doctest
         GIT_REPOSITORY https://github.com/doctest/doctest.git
-        TAG v2.4.12
+        TAG v4.2.4
     )
 
     # better_junit_reporter is provided by sparrow
